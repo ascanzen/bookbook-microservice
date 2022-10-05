bfa208a05c20a1569ffb5f8a5f3bdea17b62339c
ascanzen <ascanzen@gmail.com>
2022-10-05 12:10:38 +0800

    add commits

--- crud-service/app/api/daily_account.py
+++ crud-service/app/api/daily_account.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi_crudrouter import SQLAlchemyCRUDRouter
 
 from app.models.daily_account import DailyAccount, DailyAccountCreate, DailyAccountModel
@@ -18,8 +19,7 @@ router = SQLAlchemyCRUDRouter(
 #     return "My overloaded route that returns all the items"
 
 
-@router.put("/{item_id}")
-def overloaded_update(item_id: int, model: router.update_schema):
+def insert_or_update_imp(item_id: int, model: router.update_schema):
     try:
         res = router._get_one()(item_id)
         if res.updateTime < model.updateTime:
@@ -29,3 +29,16 @@ def overloaded_update(item_id: int, model: router.update_schema):
     except:
         # not exist
         return router._create()(model)
+
+
+@router.put("/commit/{item_id}")
+def insert_or_update(item_id: int, model: router.update_schema):
+    return insert_or_update_imp(item_id, model)
+
+
+@router.put("/commits")
+def insert_or_update(models: List[router.update_schema]):
+    for model in models:
+        item_id = model.id
+        insert_or_update_imp(item_id, model)
+    return "ok"
